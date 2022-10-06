8149ad7fce4af399640246c377e06502cae65590
chrisemenova <chrisemenova88@gmail.com>
2022-10-06 15:50:52 +0300

    Initial commit

---
+++ src/test/java/EchoTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class EchoTest {
+    @Test
+    public void shouldCheckSomethingIDontKnowWhat() {
+// Given - When - Then
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"))
+        ;
+
+    }
+}
+
